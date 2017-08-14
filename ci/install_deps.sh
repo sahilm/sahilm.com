bcbc3ed34e7343e7bca67997a3d478f95946be16
Sahil Muthoo <sahil.muthoo@gmail.com>
2017-08-14 19:20:54 +0100

    Add custom bindir to path

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -6,8 +6,9 @@ if ! which pngout; then
   PNGOUT_VERSION="20150319"
   curl -sLO http://static.jonof.id.au/dl/kenutils/pngout-${PNGOUT_VERSION}-linux.tar.gz \
     && mkdir -p /tmp/pngout \
+    && mkdir -p $HOME/bin \
     && tar xf pngout-${PNGOUT_VERSION}-linux.tar.gz -C /tmp/pngout \
-    && mv /tmp/pngout/pngout-${PNGOUT_VERSION}-linux/x86_64/pngout /usr/local/bin/
+    && mv /tmp/pngout/pngout-${PNGOUT_VERSION}-linux/x86_64/pngout $HOME/bin
 fi
 
 if ! which svgo; then
