dc86cfdc92b5c879445a928b9ea191d3a39081f9
Sahil Muthoo <sahil.muthoo@gmail.com>
2017-08-19 22:37:36 +0100

    tweak some config

--- config.rb
+++ config.rb
@@ -18,13 +18,15 @@ activate :directory_indexes
 set :markdown_engine, :redcarpet
 set :markdown, fenced_code_blocks: true, smartypants: true
 activate :syntax, line_numbers: false, css_class: 'syntax-highlight'
-activate :autoprefixer
 configure :production do
   activate :asset_hash
   activate :minify_css, inline: true
   activate :minify_javascript, inline: true
   activate :minify_html
-  activate :imageoptim
+  activate :imageoptim do |options|
+    options.manifest = false
+    options.image_extensions = %w[.png .jpg .jpeg .gif .svg]
+  end
   activate :gzip do |gz|
     gz.overwrite = ENV.key?('GZIP_NO_OVERWRITE') ? false : true
     gz.exts = %w[.css .htm .html .js .svg .xhtml .ttf .json .xml .ico .eot .otf .txt]
